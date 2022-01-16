bb4898854f775ce6aa73cc3599e501e81d004f02
Márton Borsos <borsos.marton95@gmail.com>
2022-01-16 16:25:33 +0100

    Extracted function to sign and send transactions

--- deploy.py
+++ deploy.py
@@ -15,6 +15,24 @@ def get_transaction_params() -> dict:
     return {"chainId": chain_id, "gasPrice": w3.eth.gas_price, "from": my_address, "nonce": nonce}
 
 
+def sign_and_send_transaction(transaction, wait_for_receipt: bool):
+    # Sign the transaction
+    print("Signing transaction...")
+    signed_txn = w3.eth.account.sign_transaction(
+        transaction, private_key=private_key
+    )
+    # Send it!
+    print("Sending transaction...")
+    tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+    if wait_for_receipt:
+        # Wait for the transaction to be mined, and get the transaction receipt
+        print("Waiting for transaction to finish...")
+        tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+        return tx_receipt
+
+    return None
+
+
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
 
@@ -64,14 +82,8 @@ private_key = os.getenv("PRIVATE_KEY")
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
 # Submit the transaction that deploys the contract
 transaction = SimpleStorage.constructor().buildTransaction(get_transaction_params())
-# Sign the transaction
-signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
 print("Deploying Contract!")
-# Send it!
-tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-# Wait for the transaction to be mined, and get the transaction receipt
-print("Waiting for transaction to finish...")
-tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+tx_receipt = sign_and_send_transaction(transaction, True)
 print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
 
 
@@ -79,11 +91,7 @@ print(f"Done! Contract deployed to {tx_receipt.contractAddress}")
 simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
 print(f"Initial Stored Value {simple_storage.functions.retrieve().call()}")
 greeting_transaction = simple_storage.functions.store(15).buildTransaction(get_transaction_params())
-signed_greeting_txn = w3.eth.account.sign_transaction(
-    greeting_transaction, private_key=private_key
-)
-tx_greeting_hash = w3.eth.send_raw_transaction(signed_greeting_txn.rawTransaction)
+tx_receipt = sign_and_send_transaction(greeting_transaction, True)
 print("Updating stored Value...")
-tx_receipt = w3.eth.wait_for_transaction_receipt(tx_greeting_hash)
 
 print(simple_storage.functions.retrieve().call())
